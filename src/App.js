8fe2b25d57011941c09545ddac085f7a20bd0a75
Ricardo Villagrana <ricardovillagranal@gmail.com>
2019-10-11 01:19:18 -0500

    added basic lambda function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = (props) => {
+  const [name, setName] = useState('')
+
+  const fetchData = async () => {
+    const { data } = await axios.get('/.netlify/functions/hello')
+
+    setName(data.name)
+  }
+
+  fetchData()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hi, {name}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
